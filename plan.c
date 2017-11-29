7550767af820812a37a1374aba9e2bd7b2411037
Daniella Gerard <daniella4422@gmail.com>
2017-11-29 15:34:50 -0800

    heatmap filler done

--- plan.c
+++ plan.c
@@ -59,26 +59,51 @@ void				assess_weaknesses(t_duel *duel, int start_x, int start_y) //assign the c
 
 	x = start_x;
 	y = start_y;
-	while (y < duel->arena_y && duel->risk[0] == -1)
-		if (duel->arena[y][x] == -1 || duel->arena[y++][x] == -2) //make sure this incs correctly
-			duel->risk[0] = (duel->arena[y][x] == -2) ? y - start_y + 2 : y - start_y; //+2 to favor recent growth
+	while (y < duel->arena_y && duel->risk[0] == 0)
+		if (duel->arena[y][x] == -1 || duel->arena[y++][x] == -2) //make sure incing here
+			duel->risk[0] = (duel->arena[y][x] == -2) ? y - start_y + 3 : y - start_y + 1; //+3 to favor recent growth, +1 to make sure that loop is exited
 	x = start_x;
 	y = start_y;
-	while (y < duel->arena_y && duel->risk[4] == -1)
+	while (y < duel->arena_y && duel->risk[4] == 0)
 		if (duel->arena[y][x] == -1 || duel->arena[y--][x] == -2)
-			duel->risk[0] = (duel->arena[y][x] == -2) ? start_y - y + 2 : start_y - y;
+			duel->risk[4] = (duel->arena[y][x] == -2) ? start_y - y + 3 : start_y - y + 1;
 	x = start_x;
 	y = start_y;
-	while (x < duel->arena_x && duel->risk[2] == -1)
-		if (duel->arena[y][x] == -1 || duel->arena[y++][x] == -2) //make sure this incs correctly
-			duel->risk[0] = (duel->arena[y][x] == -2) ? y - start_y + 2 : y - start_y; //+2 to favor recent growth
-
+	while (x < duel->arena_x && duel->risk[2] == 0)
+		if (duel->arena[y][x] == -1 || duel->arena[y][x++] == -2)
+			duel->risk[2] = (duel->arena[y][x] == -2) ? x - start_x + 3 : x - start_x + 1;
+	x = start_x;
+	y = start_y;
+	while (x < duel->arena_x && duel->risk[6] == 0)
+		if (duel->arena[y][x] == -1 || duel->arena[y][x--] == -2)
+			duel->risk[6] = (duel->arena[y][x] == -2) ? start_x - x + 3 : start_x - x + 1;
+	x = start_x;
+	y = start_y;
+	while (y < duel->arena_y && x < duel->arena_x && duel->risk[1] == 0)
+		if (duel->arena[y][x] == -1 || duel->arena[y++][x++] == -2)
+			duel->risk[1] = (duel->arena[y][x] == -2) ? y - start_y + 3 : y - start_y + 1;
+	x = start_x;
+	y = start_y;
+	while (y < duel->arena_y && x < duel->arena_x && duel->risk[3] == 0)
+		if (duel->arena[y][x] == -1 || duel->arena[y--][x++] == -2)
+			duel->risk[3] = (duel->arena[y][x] == -2) ? x - start_x + 3 : x - start_x + 1;
+	x = start_x;
+	y = start_y;
+	while (y < duel->arena_y && x < duel->arena_x && duel->risk[5] == 0)
+		if (duel->arena[y][x] == -1 || duel->arena[y--][x--] == -2)
+			duel->risk[5] = (duel->arena[y][x] == -2) ? start_x - x + 3 : start_x - x + 1;
+	x = start_x;
+	y = start_y;
+	while (y < duel->arena_y && x < duel->arena_x && duel->risk[7] == 0)
+		if (duel->arena[y][x] == -1 || duel->arena[y++][x--] == -2)
+			duel->risk[7] = (duel->arena[y][x] == -2) ? y - start_y + 3 : y - start_y + 1;
 }
 
 void				scan_enemy(t_duel *duel)// go through the map, and if not colonized by me, re-assess number
 {
 	int		y;
 	int		x;
+	int		i;
 
 	y = 0;
 	while (y > duel->arena_y)
@@ -87,7 +112,15 @@ void				scan_enemy(t_duel *duel)// go through the map, and if not colonized by m
 		while (x > duel->arena_x)
 		{
 			if (arena[y][x] > -1)
+			{
+				//reinitialize here for risk
+				i = 0;
 				assess_weaknesses(duel, x, y);
+				while (i < 8)
+				{
+					risk[i] =
+				}
+			}
 			x++;
 		}
 		y++;
@@ -96,6 +129,7 @@ void				scan_enemy(t_duel *duel)// go through the map, and if not colonized by m
 
 void				plan(t_duel *duel)
 {
+	//malloc here for risk
 	scan_enemy(duel);
 	find_opening(duel); //look for the lowest number where you can place a piece and save coords for attack in move int array
 }
